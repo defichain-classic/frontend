479e9804a5e427c836305ffb9d723c93f2bd38f4
Defichain Classic Collective <opensource@defichain-classic.com>
2023-12-12 14:24:39 +1000

    no RPC API 2

--- ui/pages/Home.tsx
+++ ui/pages/Home.tsx
@@ -8,8 +8,6 @@ import LatestZkEvmL2Batches from 'ui/home/LatestZkEvmL2Batches';
 import Stats from 'ui/home/Stats';
 import Transactions from 'ui/home/Transactions';
 import AdBanner from 'ui/shared/ad/AdBanner';
-import ProfileMenuDesktop from 'ui/snippets/profileMenu/ProfileMenuDesktop';
-import SearchBar from 'ui/snippets/searchBar/SearchBar';
 
 const Home = () => {
   return (
